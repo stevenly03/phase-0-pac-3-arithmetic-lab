4aab2930fc89409380e7716f24045bd1fe5f2ca7
Steven Ly <steven.ly10213@gmail.com>
2021-03-06 19:05:22 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(num1,num2){
+    return num1+num2;
+}
+add(1+1)
+function subtract(num1,num2){
+    return num1-num2;
+}
+subtract(2-1)
+function multiply(num1,num2){
+    return num1*num2;
+}
+multiply(2*2)
+function divide(num1,num2){
+    return num1/num2
+}
+divide(4/2)
+
+function increment(n){
+    return n+= 1;
+}
+increment()
+
+function decrement(n){
+    return n-= 1;
+}
+decrement()
+
+function makeInt(n, base){
+    return parseInt(n, base = 10)
+}
+makeInt()
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
+preserveDecimal()
